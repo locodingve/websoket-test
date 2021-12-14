4f100111d7fb2f73d3a05efc0d0b37e54dfcdae9
yunyun <locodingve@gmail.com>
2021-12-14 14:53:01 +0900

    feat : test 코드

---
+++ src/main/java/com/test/websoket/controller/SlamEventListener.java
@@ -0,0 +1,7 @@
+package com.test.websoket.controller;
+
+/**
+ * Created by yunyun on 2021/12/13.
+ */
+public class SlamEventListener {
+}
---
+++ src/main/java/com/test/websoket/model/MessageType.java
@@ -0,0 +1,9 @@
+package com.test.websoket.model;
+
+/**
+ * Created by yunyun on 2021/12/13.
+ */
+public enum MessageType {
+    FOLLOW,
+    LOUDSPEAKER
+}
